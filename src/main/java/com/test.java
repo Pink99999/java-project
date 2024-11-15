212e42fe56c7ca1e693f1dcb589422ff0a5604a5
dev <pawardevyani@gmail.com>
2024-11-15 23:32:34 +0530

    first commit

---
+++ src/main/java/com/test.java
@@ -0,0 +1,14 @@
+package com;
+
+public class test {
+    public static void main(String[] args) {
+        test t=new test();
+        t.myfunc();
+
+    }
+    public void myfunc() {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("Hello" + i);
+        }
+    }
+}
